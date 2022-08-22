c506af5dd7b40ad2acdcd817b3202b80490b78a2
joshiemoore <jxm5210@g.rit.edu>
2022-08-22 00:23:16 -0400

    Add github URL

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ def main():
         author="Joshua Moore",
         author_email="jxm5210@g.rit.edu",
         license="GPLv2",
+        url="https://github.com/joshiemoore/ludicrous/",
         ext_modules=[
             Extension(
                 "ludicrous",
